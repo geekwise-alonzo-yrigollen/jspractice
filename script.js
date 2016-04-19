46e2244dab78fb4326aad39387d87119cfc2505c
Alonzo Yrigollen <geekwise.alonzo.yrigollen@gmail.com>
2016-04-18 22:23:15 -0700

    se

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log{'hello from the js file'};
+function createTable(numCols, numRows){
+    
+    var begin = '<table>';
+    var middle = '';
+    var end = '<table>';
+    for (var r = 0; r < numRows; r++){
+        
+        middle += '<tr>';
+    for (var c = 0; c < numCols; c++){
+        
+        middle += '<td></td>';
+          
+    }
+        middle += '</tr>';
+    }
+     return (begin + middle + end);
+}
+
+document.body.innerHTML = createTable(25, 20);
+
