9614e119a6a22532350ecbf6d4a60bcfc5a0ea73
Alonzo Yrigollen <geekwise.alonzo.yrigollen@gmail.com>
2016-04-20 12:46:49 -0700

    w
    a

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+
+
+
+
+
+
+
+
 function createTable(numCols, numRows){
     
     var begin = '<table>';
     var middle = '';
-    var end = '<table>';
+    var end = '</table>';
     for (var r = 0; r < numRows; r++){
         
         middle += '<tr>';
     for (var c = 0; c < numCols; c++){
         
-        middle += '<td>AAY</td>';
+        middle += '<td>AY</td>';
         
           
     }
@@ -21,7 +29,7 @@ function createTable(numCols, numRows){
     
 }
 
-document.body.innerHTML = createTable(30, 20);
+document.body.innerHTML = createTable(30, 11);
 //document.getElementByTagName('td');
 //document.onkeydown = function(){
 //    var randomCell = Math.floor
